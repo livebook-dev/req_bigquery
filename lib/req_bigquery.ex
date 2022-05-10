77d350a0ce9b633c6ab37f81c5e51ee061bafdd5
Alexandre de Souza <alexandre@aledsz.com.br>
2022-05-10 12:55:31 -0300

    Implement the request step for Google BigQuery

--- lib/req_bigquery.ex
+++ lib/req_bigquery.ex
@@ -1,2 +1,84 @@
 defmodule ReqBigQuery do
+  # TODO: Add docs
+  @moduledoc false
+
+  alias Req.Request
+
+  @base_url "https://bigquery.googleapis.com/bigquery/v2"
+
+  @doc false
+  @spec attach(Request.t(), keyword()) :: Request.t()
+  def attach(%Request{} = request, options \\ []) do
+    request
+    |> Request.prepend_request_steps(bigquery_run: &bigquery_run/1)
+    |> Request.prepend_response_steps(bigquery_parser: &bigquery_parser/1)
+    |> Request.register_option(:goth)
+    |> Request.register_option(:dataset)
+    |> Request.register_option(:project_id)
+    |> Request.register_option(:bigquery)
+    |> Request.register_option(:bigquery_params)
+    |> merge_options(options)
+  end
+
+  defp merge_options(request, options) do
+    Map.update!(request, :options, fn opts ->
+      options
+      |> Enum.into(%{})
+      |> Map.merge(opts)
+    end)
+  end
+
+  @doc false
+  def query!(%Request{options: options} = request, query, opts \\ []) do
+    options =
+      opts
+      |> Enum.into(%{})
+      |> Map.merge(options)
+      |> Map.put_new(:bigquery, query)
+
+    Req.post!(%{request | options: options})
+  end
+
+  defp api_url(options) do
+    "#{@base_url}/projects/#{options[:project_id]}/queries"
+  end
+
+  defp bigquery_run(%Request{} = request) do
+    request
+    |> put_url()
+    |> put_goth_token()
+    |> put_encoded_body()
+  end
+
+  defp put_url(%{options: options} = request) do
+    url = api_url(options)
+    %{request | url: URI.parse(url)}
+  end
+
+  # TODO: Add Req.Request.put_header/3
+  defp put_goth_token(%{options: options} = request) do
+    goth = Map.fetch!(options, :goth)
+    token = Goth.fetch!(goth).token
+
+    update_in(request.headers, &[{"authorization", "Bearer #{token}"} | &1])
+  end
+
+  # TODO: Add Req.Request.put_body/2
+  defp put_encoded_body(%{options: options} = request) do
+    dataset = Map.fetch!(options, :dataset)
+    query = Map.fetch!(options, :bigquery)
+
+    json = %{
+      defaultDataset: %{
+        datasetId: dataset
+      },
+      query: query
+    }
+
+    update_in(request.body, fn _ -> Jason.encode!(json) end)
+  end
+
+  defp bigquery_parser({request, response}) do
+    {request, response}
+  end
 end
