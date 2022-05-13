5e74b0141a77d11ec80082848d14c6753c39a105
Alexandre de Souza <alexandre@aledsz.com.br>
2022-05-13 12:41:07 -0300

    Implement response decoder for BigQuery (#7)
    * Add result struct for BigQuery response
    
    * Add function to decode response body
    
    * Add table dependency as optional

--- lib/req_bigquery.ex
+++ lib/req_bigquery.ex
@@ -2,6 +2,7 @@ defmodule ReqBigQuery do
   @moduledoc false
 
   alias Req.Request
+  alias ReqBigQuery.Result
 
   @allowed_options ~w(goth dataset project_id bigquery)a
   @base_url "https://bigquery.googleapis.com/bigquery/v2"
@@ -10,6 +11,7 @@ defmodule ReqBigQuery do
   def attach(%Request{} = request, options \\ []) do
     request
     |> Request.prepend_request_steps(bigquery_run: &run/1)
+    |> Request.append_response_steps(bigquery_decode: &decode/1)
     |> Request.register_options(@allowed_options)
     |> Request.merge_options(options)
   end
@@ -34,4 +36,43 @@ defmodule ReqBigQuery do
       request
     end
   end
+
+  defp decode({request, %{status: 200} = response}) do
+    {request, update_in(response.body, &decode_body/1)}
+  end
+
+  defp decode(any), do: any
+
+  defp decode_body(%{
+         "jobReference" => %{"jobId" => job_id},
+         "kind" => "bigquery#queryResponse",
+         "rows" => rows,
+         "schema" => %{"fields" => fields},
+         "totalRows" => num_rows
+       }) do
+    %Result{
+      job_id: job_id,
+      num_rows: String.to_integer(num_rows),
+      rows: prepare_rows(rows, fields),
+      columns: prepare_columns(fields)
+    }
+  end
+
+  defp prepare_rows(rows, fields) do
+    Enum.map(rows, fn %{"f" => columns} ->
+      Enum.with_index(columns, fn %{"v" => value}, index ->
+        field = Enum.at(fields, index)
+        convert_value(value, field)
+      end)
+    end)
+  end
+
+  defp prepare_columns(fields) do
+    Enum.map(fields, & &1["name"])
+  end
+
+  defp convert_value(value, %{"type" => "FLOAT"}), do: String.to_float(value)
+  defp convert_value(value, %{"type" => "INTEGER"}), do: String.to_integer(value)
+  defp convert_value(value, %{"type" => "BOOLEAN"}), do: String.downcase(value) == "true"
+  defp convert_value(value, _), do: value
 end
---
+++ lib/req_bigquery/result.ex
@@ -0,0 +1,30 @@
+defmodule ReqBigQuery.Result do
+  @moduledoc """
+  Result struct returned from any successful query.
+
+  Its fields are:
+
+    * `columns` - The column names;
+    * `rows` - The result set. A list of lists, each inner list corresponding to a
+      row, each element in the inner list corresponds to a column;
+    * `num_rows` - The number of fetched or affected rows;
+    * `job_id` - The ID of the Google BigQuery's executed job.
+  """
+
+  @type t :: %__MODULE__{
+          columns: [String.t()],
+          rows: [[term()] | binary()],
+          num_rows: non_neg_integer(),
+          job_id: binary()
+        }
+
+  defstruct [:job_id, num_rows: 0, rows: [], columns: []]
+end
+
+if Code.ensure_loaded?(Table.Reader) do
+  defimpl Table.Reader, for: ReqBigQuery.Result do
+    def init(result) do
+      {:rows, %{columns: result.columns}, result.rows}
+    end
+  end
+end
