d0f761566453c32fdfd79040c411ae150f792514
Alexandre de Souza <alexandre@aledsz.com.br>
2022-05-16 14:22:58 -0300

    Add ReqBigQuery module docs

--- lib/req_bigquery.ex
+++ lib/req_bigquery.ex
@@ -1,5 +1,35 @@
 defmodule ReqBigQuery do
-  @moduledoc false
+  @moduledoc """
+  The Google BigQuery plugin for [Req](https://github.com/wojtekmach/req).
+
+  ReqBigQuery uses [Goth](https://github.com/peburrows/goth) to generate the
+  OAuth2 Token from Google Credentials, but we don't start the Goth server,
+  we only retrieve the token from given Goth server name.
+
+  This plugin also provides a `ReqBigQuery.Result` struct to normalize the
+  result from Google BigQuery API and allows the result to be rendered by
+  [table](https://github.com/dashbitco/table).
+
+  ## Plugin Options
+
+    * `:goth` - the goth server name.
+
+    * `:project_id` - the project id from Google Cloud Platform.
+
+    * `:dataset` - the dataset from Google BigQuery.
+
+    * `:bigquery` - the statement to be executed by Google's API.
+
+  Every option above is required by default. (for more information about the options, see attach/2)
+
+  ## Examples
+
+      iex> req = Req.new() |> ReqBigQuery.attach(goth: MyGoth, project_id: "foo", dataset: "bar")
+      iex> Req.post!(req, bigquery: "SELECT * FROM iris LIMIT 5")
+      %Req.Request{body: %ReqBigQuery.Result{}, status: 200}
+
+  For more information and examples, please see our `Guides` page.
+  """
 
   alias Req.Request
   alias ReqBigQuery.Result
@@ -7,6 +37,28 @@ defmodule ReqBigQuery do
   @allowed_options ~w(goth dataset project_id bigquery)a
   @base_url "https://bigquery.googleapis.com/bigquery/v2"
 
+  @doc """
+  Attaches the steps from this plugin into an existing Req's request struct.
+
+  It adds the request step to prepare Req to know what URL it should use,
+  the request body and the authorization header with the token provided by Goth.
+  It allows the user to configure Req with our allowed options (see section below).
+
+  The response from Google's API will be decoded into `ReqBigQuery.Result` struct.
+
+  ## Options
+
+    * `:goth` - The provided name for `Goth` library, it should be an atom.
+
+    * `:project_id` - the id from the project created on Google Cloud Platform,
+      it should be a string.
+
+    * `:dataset` - the dataset from Google BigQuery, allowing the plugin to execute
+      the query in the correct dataset and it should be a string.
+
+    * `:bigquery` - the query to be exexcuted by Google BigQuery, it should be a string.
+
+  """
   @spec attach(Request.t(), keyword()) :: Request.t()
   def attach(%Request{} = request, options \\ []) do
     request
