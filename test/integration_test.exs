d42a11fa4c99d48e3356d1ad81846b0c6079b169
Alexandre de Souza <alexandre@aledsz.com.br>
2022-05-17 17:04:20 -0300

    Update integration test

--- test/integration_test.exs
+++ test/integration_test.exs
@@ -13,22 +13,38 @@ defmodule IntegrationTest do
     source = {:service_account, credentials, []}
     start_supervised!({Goth, name: goth, source: source, http_client: &Req.request/1})
 
+    query = """
+    SELECT title, SUM(views) AS views
+      FROM `bigquery-public-data.wikipedia.table_bands`
+     WHERE EXTRACT(YEAR FROM datehour) <= 2021
+     GROUP BY title
+     ORDER BY views DESC
+     LIMIT 10
+    """
+
     response =
       Req.new()
       |> ReqBigQuery.attach(project_id: project_id, goth: goth)
-      |> Req.post!(
-        bigquery: "SELECT id, text FROM [bigquery-public-data:hacker_news.full] LIMIT 2"
-      )
+      |> Req.post!(bigquery: query)
 
     assert response.status == 200
+
     result = response.body
-    assert %ReqBigQuery.Result{} = result
-    assert result.num_rows == 2
-    assert result.columns == ["id", "text"]
-    assert [[x1, y1], [x2, y2]] = result.rows
-    assert is_integer(x1)
-    assert is_integer(x2)
-    assert is_binary(y1)
-    assert is_binary(y2)
+
+    assert result.columns == ["title", "views"]
+    assert result.num_rows == 10
+
+    assert result.rows == [
+             ["The_Beatles", 13_758_950],
+             ["Queen_(band)", 12_019_563],
+             ["Pink_Floyd", 9_522_503],
+             ["AC/DC", 8_972_364],
+             ["Led_Zeppelin", 8_294_994],
+             ["Linkin_Park", 8_242_802],
+             ["The_Rolling_Stones", 7_825_952],
+             ["Red_Hot_Chili_Peppers", 7_302_904],
+             ["Fleetwood_Mac", 7_199_563],
+             ["Twenty_One_Pilots", 6_970_692]
+           ]
   end
 end
